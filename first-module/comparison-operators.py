b4ffde5c51db032822245a5920e77448631fd2e4
¨soutbiaafnts¨ <bianca.fontes.dev@gmail.com>
2026-09-15 19:38:23 -0300

    comparison operators

---
+++ first-module/comparison-operators.py
@@ -0,0 +1,15 @@
+# igualdade
+withdraw =  450
+balance = 200
+print(withdraw == balance)  # False 
+
+# diferença
+print(withdraw != balance)  # True
+
+# maior que || maior ou igual a
+print(withdraw > balance)  # True
+print(withdraw >= balance)  # True
+
+# menor que || menor ou igual a
+print(withdraw < balance)  # False
+print(withdraw <= balance)  # False

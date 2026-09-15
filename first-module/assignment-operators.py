a3710501af4d20116dbfbb3d2d1b848f1244422b
¨soutbiaafnts¨ <bianca.fontes.dev@gmail.com>
2026-09-15 20:40:26 -0300

    assignment operators

---
+++ first-module/assignment-operators.py
@@ -0,0 +1,35 @@
+# Operadores de Atribuição são operadores utilizados para definir o valor inicial ou sobrescrever o valor de uma variável
+
+# atribuição simples
+balance = 500
+print(balance) # saída: 500
+
+# atribuição com adição
+balance += 200
+print(balance) # saída: 700
+
+# atribuição com subtração
+balance -= 100
+print(balance) # saída: 600
+
+# atribuição com multiplicação
+balance *= 2
+print(balance) # saída: 1200
+
+# atribuição com divisão
+balance /= 4
+print(balance) # saída: 300.0
+
+balance = 300
+
+# divisão inteira
+balance //= 3
+print(balance) # saída: 100
+
+# atribuição com módulo
+balance %= 30
+print(balance) # saída: 10
+
+# atribuição com exponenciação
+balance **= 3
+print(balance) # saída: 1000
